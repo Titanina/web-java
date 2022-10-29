459a7bc3e306f35cbaf0f3771aff7dca17999030
Titanina <111214348+Titanina@users.noreply.github.com>
2022-10-29 21:17:25 +0900

    Add files via upload

---
+++ ch05.study/snippet01-study.js
@@ -0,0 +1,6 @@
+var name = "answer";
+var obj = {
+[name] : 42
+};
+console.log(obj); // {answer : 42}
+
---
+++ ch05.study/snippet02-study.js
@@ -0,0 +1,7 @@
+let prefix = "ans";
+let obj = {
+  [prefix + "wer"]: 42
+
+};
+
+console.log(obj); // {answer: 42}
