06700211a8b369eaaffd401402d56f1f997035f7
Titanina <111214348+Titanina@users.noreply.github.com>
2022-11-02 18:21:55 +0900

    Update snippet12-study.js

---
+++ ch05.study/snippet12-study.js
@@ -0,0 +1,13 @@
+const obj =  {
+  toString() {
+    return super.toString(). toUpperCase();
+  }
+};
+
+Object.setPrototypeOf(obj, {
+  toString() {
+    return "a different string";
+  }
+});
+console.log(obj.toString()); // "A DIFFERENT STRING"
+
